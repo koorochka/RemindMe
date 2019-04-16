5c0651234e2f4365a88d221dc75cafd8a3b76d02
Koorochka <artemij@koorochka.ru>
2019-04-16 15:08:31 +0300

    Created basic project structure

---
+++ app/src/main/java/com/koorochka/remindme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.koorochka.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ativity_main);
+    }
+}
